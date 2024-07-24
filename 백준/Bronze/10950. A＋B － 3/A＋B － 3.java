e95ad1512a2c3373398dabc57fff535e4d261008
yeoon <52392720+yeooniyeoon@users.noreply.github.com>
2024-07-24 19:18:26 +0900

    [Bronze V] Title: A+B - 3, Time: 232 ms, Memory: 17892 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt();
+        for (int i = 0; i < count; i++)
+            System.out.println(sc.nextInt() + sc.nextInt());
+    }
+}
