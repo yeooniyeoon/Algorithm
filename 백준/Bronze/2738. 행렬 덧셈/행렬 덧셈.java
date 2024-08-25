f47c7ea15a5610d5e1444008d81158804feb9d6b
yeoon <52392720+yeooniyeoon@users.noreply.github.com>
2024-08-25 23:05:06 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 280 ms, Memory: 18156 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -1,22 +1,47 @@
-import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int row = sc.nextInt();
-        int col = sc.nextInt();
-        Queue<Integer> matrixA = new LinkedList<>();
-        Queue<Integer> matrixB = new LinkedList<>();
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        for (int i = 0; i < row * col; i++)
-            matrixA.add(sc.nextInt());
-        for (int i = 0; i < row * col; i++)
-            matrixB.add(sc.nextInt());
+    public static void main(String[] args) throws IOException {
+        String[] matrixSize = br.readLine().split(" ");
 
+        int row = Integer.parseInt(matrixSize[0]);
+        int col = Integer.parseInt(matrixSize[1]);
+
+        int[][] matrixA = new int[row][col];
+        int[][] matrixB = new int[row][col];
+        int[][] resultMatrix = new int[row][col];
+
+        matrixA = readMatrix(row, col, matrixA);
+        matrixB = readMatrix(row, col, matrixB);
+
+        resultMatrix = addMatrix(row, col, matrixA, matrixB);
+        printMatrix(row, col, resultMatrix);
+    }
+
+    public static int[][] readMatrix(int row, int col, int[][] matrix) throws IOException {
         for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                System.out.print(matrixA.poll() + matrixB.poll() + " ");
-            }
+            String[] input = br.readLine().split(" ");
+            for (int j = 0; j < input.length; j++)
+                matrix[i][j] = Integer.parseInt(input[j]);
+        }
+        return matrix;
+    }
+
+    public static int[][] addMatrix(int row, int col, int matrixA[][], int matrixB[][]) {
+        int[][] resultMatrix = new int[row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++)
+                resultMatrix[i][j] = matrixA[i][j] + matrixB[i][j];
+        }
+        return resultMatrix;
+    }
+
+    public static void printMatrix(int row, int col, int[][] matrix) {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++)
+                System.out.print(matrix[i][j] + " ");
             System.out.println();
         }
     }
